ac4d1d74c6cbd4436fd8121cc6a4cfd9483f5f90
Bourbz <bourbonplaytime@gmail.com>
2018-07-12 10:58:42 -0400

    added button and tweaked styling

--- Buildtheflockonline.js
+++ Buildtheflockonline.js
@@ -1,3 +1,4 @@
 $('.js').hide().fadeIn(5000).delay(5000).slideUp(5000);
-$('.Lsup').delay(15000).fadeOut(15000);
+$('.Lsup').delay(15000).fadeOut(15000).delay(30000).fadeIn(5000);
 $('.Hendo').hide().delay(30000).fadeIn(7500).fadeOut(7500);
+$('.Green').hide().delay(45000).fadeIn(7500).fadeOut(7500);
