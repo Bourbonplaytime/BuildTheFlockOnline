1e5bd780c0f9a98ab9a674c3c7ac54db50ec82af
Bourbz <bourbonplaytime@gmail.com>
2018-07-18 19:01:18 -0400

    repopulated 'birdswithteeth.html' and deleted bad javascript on lsup etc

--- Lsup.js
+++
@@ -1,6 +0,0 @@
-const expText = document.querySelectorAll('.enlarge');
-
-expText.addEventListener('mouseover', () => {
-  document.querySelectorAll('.enlarge').style.height="150px";
-  document.querySelectorAll('.enlarge').style.width="150px";
-});
