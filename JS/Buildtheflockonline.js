f0098c9889a0542b895e7cae0eeaec8613d79728
Bourbz <bourbonplaytime@gmail.com>
2018-08-21 23:26:04 -0400

    Went all caps on #BTF, added an image to contacts page, played with footers

--- JS/Buildtheflockonline.js
+++ JS/Buildtheflockonline.js
@@ -1,7 +1,7 @@
 function loop() {
   $('.js').hide().fadeIn(2500).delay(2500).slideUp(2500);
   $('.Lsup').delay(7500).fadeOut(7500).delay(14999).fadeIn(1);
-  $('.Hendo').hide().delay(15000).fadeIn(3750).fadeOut(3750);
-  $('.Green').hide().delay(22500).fadeIn(3750).fadeOut(3749);}
+  $('.CutTheNets').hide().delay(15000).fadeIn(3750).fadeOut(3750);
+  $('.RedLogo').hide().delay(22500).fadeIn(3750).fadeOut(3749);}
 
   loop();
